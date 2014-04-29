ecda57f91c878927657e7553b1c21b26ea076231
Mike Edwards <mikeedwards83@googlemail.com>
2014-04-29 07:39:18 +0100

    cache config work

---
+++ Source/Glass.Mapper.Sc/Caching/CacheClearEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Mapper.Caching;
+
+namespace Glass.Mapper.Sc.Caching
+{
+    public class CacheClearEvent
+    {
+        public void ClearCache(object sender, EventArgs args)
+        {
+            foreach (var contextPair in Context.Contexts)
+            {
+                try
+                {
+                    Sitecore.Diagnostics.Log.Info("Glass Cache Clearing Context {0}".Formatted(contextPair.Key), this);
+                    var context = contextPair.Value;
+
+                    var cacheManager = context.DependencyResolver.Resolve<ICacheManager>();
+
+                    if (cacheManager == null)
+                    {
+                        Sitecore.Diagnostics.Log.Info("Glass Cache Manager not set for {0}".Formatted(contextPair.Key),
+                            this);
+                    }
+                    else
+                    {
+                        cacheManager.ClearCache();
+                        Sitecore.Diagnostics.Log.Info("Glass Cache Manager cleared for {0}".Formatted(contextPair.Key),
+                            this);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Sitecore.Diagnostics.Log.Error("Glass Cache Manager error for {0}.".Formatted(contextPair.Key), ex,
+                            this);
+                }
+
+            }
+
+        }
+    }
+}
